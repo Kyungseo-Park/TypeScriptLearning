c06aa9b9923aa0e5985d7f16fce1d68b5cf8f00f
KyungseoPark <troublesome.dev@gmail.com>
2021-01-03 14:45:03 +0900

    1-2 목록 표시하기

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +13,16 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'winstom'
   };
+  heroes = HEROES;
+
+  selectedHero: Hero | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  } 
 }
